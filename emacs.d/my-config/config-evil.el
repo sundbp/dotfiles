5d8276d52b13b5167e3ec5280e28e9774a362106
Patrik Sundberg <patrik.sundberg@gmail.com>
2012-10-02 19:11:12 +0100

    Add evil-leader submodule and start to add keybindings.

--- emacs.d/my-config/config-evil.el
+++ emacs.d/my-config/config-evil.el
@@ -4,3 +4,11 @@
 (require 'evil)
 (evil-mode 1)
 
+(add-to-list 'load-path "~/.emacs.d/my-pkgs/evil-leader")
+(require 'evil-leader)
+
+;ffip binding
+(let ((maps (list evil-normal-state-map evil-insert-state-map evil-visual-state-map)))
+ (dolist (map maps)
+   (define-key map "\C-p" 'find-file-in-project)
+   (define-key map "\C-b" 'ido-switch-buffer)))
---
+++ emacs.d/my-config/config-keybindings.el
@@ -0,0 +1,3 @@
+;ffip
+(global-unset-key "\C-p")
+(global-set-key (kbd "C-p") 'find-file-in-project)
