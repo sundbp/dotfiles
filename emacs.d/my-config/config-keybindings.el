70b51bbdb9e40c96dd2d6d05638224cf01a021f4
Patrik Sundberg <patrik.sundberg@gmail.com>
2012-11-27 17:30:57 +0000

    Fix keybindings for ctrol-arrowkeys in emacs (tmux or no tmux)

--- emacs.d/my-config/config-keybindings.el
+++ emacs.d/my-config/config-keybindings.el
@@ -6,4 +6,12 @@
 (global-unset-key "\C-b")
 (global-set-key (kbd "C-b") 'ido-switch-buffer)
 
-;; make sure that up-down-left-right works in terminal
+;; make sure that Ctrl+up|down|left|right works in terminal
+(define-key input-decode-map "\eOa" [(control up)])
+(define-key input-decode-map "\e[1;5A" [(control up)])
+(define-key input-decode-map "\eOb" [(control down)])
+(define-key input-decode-map "\e[1;5B" [(control down)])
+(define-key input-decode-map "\eOd" [(control left)])
+(define-key input-decode-map "\e[1;5D" [(control left)])
+(define-key input-decode-map "\eOc" [(control right)])
+(define-key input-decode-map "\e[1;5C" [(control right)])
--- emacs.d/my-config/config-nrepl.el
+++ emacs.d/my-config/config-nrepl.el
@@ -5,4 +5,6 @@
 
 (add-to-list 'same-window-buffer-names "*nrepl*")
 
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+
 (setq nrepl-connected-hook (reverse nrepl-connected-hook))
