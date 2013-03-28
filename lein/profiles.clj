a65147986e03ad0d5d7b92c0a52633a9a476835f
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-03-28 23:04:13 +0000

    Upgrade lein and clean up profiles.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,17 +1,5 @@
-{:user {:plugins [;;[lein-ritz "0.6.0"]
-                  ;;[lein-pedantic "0.0.3"]
-                  ;;[lein-localrepo "0.4.1"]
-                  [lein-pprint "1.1.1"]
-                  ;;[lein-immutant "0.11.0"]
-                  ]
-        :dependencies [;;[ritz/ritz-nrepl-middleware "0.6.0"]
-                       ;;[ritz/ritz-debugger "0.6.0"]
-                       ;;[ritz/ritz-repl-utils "0.6.0"]
-                       [org.clojure/tools.nrepl "0.2.2"]
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [lein-immutant "0.17.1"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.2"]
                        [slamhound "1.3.1"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        ;;:repl-options {:nrepl-middleware [ritz.nrepl.middleware.javadoc/wrap-javadoc
-        ;;                                  ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}
-                                                    }
- ;;:hooks [ritz.add-sources]
-                  }
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
