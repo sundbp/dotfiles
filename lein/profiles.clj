dd0779eab4f86d16133970a7d51ef7a2991bac89
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-04-10 10:17:07 +0000

    repo config for lein.
    Add our local mirrors to mirrors, and our own snapshots/releases as well.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,13 @@
                   [lein-immutant "0.17.1"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.2"]
                        [slamhound "1.3.1"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :repositories
+        [["snapshots" {:url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/snapshots/"
+                       :update :always}]
+         ["releases" {:url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/releases/"
+                      :update :always}]]
+        :mirrors {"central" {:name "Internal nexus mirror of central"
+                             :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/central/"}
+                  #"clojars" {:name "Internal nexus mirror of clojars"
+                              :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/clojars/"}}}}
