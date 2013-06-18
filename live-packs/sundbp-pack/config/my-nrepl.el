1521ab00828a9c0377928ab12c0a32b51424c672
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-06-18 17:12:48 +0100

    Add keybinding for doing a reset (in our clojure workflow).

--- live-packs/sundbp-pack/config/my-nrepl.el
+++ live-packs/sundbp-pack/config/my-nrepl.el
@@ -1,5 +1,17 @@
+(require 'nrepl)
+
 (custom-set-variables '(nrepl-use-pretty-printing t)
                       '(nrepl-history-file "~/.nrepl-history"))
 
 (require 'nrepl-inspect)
 (define-key nrepl-mode-map (kbd "C-c C-i") 'nrepl-inspect)
+
+(defun nrepl-reset ()
+  (interactive)
+  (set-buffer "*nrepl*")
+  (goto-char (point-max))
+  (insert "(user/reset)")
+  (nrepl-return)
+  (message "Started system refresh via tools.namespace."))
+
+(global-set-key (kbd "C-c M-k") 'nrepl-reset)
