d4738072dfe2317c1d3979bbff2c3354d96f7a27
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-07-25 08:34:00 +0000

    add deploy repositories.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -6,6 +6,11 @@
         :repl-options {:nrepl-middleware [inspector.middleware/wrap-inspect]}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :mirrors {"central" {:name "central-proxy"
-                             :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/central/"}
-                  #"clojars" {:name "clojars-proxy"
-                              :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/clojars/"}}}}
+                            :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/central/"}
+                 #"clojars" {:name "clojars-proxy"
+                             :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/clojars/"}}
+        :deploy-repositories [["snapshots" {:url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/snapshots/"
+                                            :update :always}]
+                              ["releases" {:url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/releases/"
+                                           :update :always}]]
+        }}
