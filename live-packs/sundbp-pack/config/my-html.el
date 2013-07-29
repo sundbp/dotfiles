c501e286e15f7f438d1b5335c41fde14d6dbc92b
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-07-29 17:39:11 +0000

    don't use the autocomplete on tags - use emmet-mode instead.

--- live-packs/sundbp-pack/config/my-html.el
+++ live-packs/sundbp-pack/config/my-html.el
@@ -2,7 +2,7 @@
   '(progn
      (require 'tagedit)
      (tagedit-add-paredit-like-keybindings)
-     (tagedit-add-experimental-features)
+     ;;(tagedit-add-experimental-features)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))
      (define-key html-mode-map (kbd "s-k") 'tagedit-kill-attribute)
      (define-key html-mode-map (kbd "s-<return>") 'tagedit-toggle-multiline-tag)))
