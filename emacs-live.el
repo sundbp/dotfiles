5bc71aa42d1c395d773d9804b974a8fa216a6c8c
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-09-04 10:21:10 +0100

    Modify the load order to reflect same as upstream.

--- emacs-live.el
+++ emacs-live.el
@@ -1,9 +1,9 @@
 (live-use-packs '(live/foundation-pack
-                   live/power-pack
-                   live/git-pack
-                   live/clojure-pack
-                   live/lang-pack
-                   live/org-pack
-                   live/bindings-pack))
+                  live/clojure-pack
+                  live/lang-pack
+                  live/power-pack
+                  live/git-pack
+                  live/org-pack
+                  live/bindings-pack))
 
 (live-add-packs '(~/.live-packs/solarized-pack ~/.live-packs/sundbp-pack))
