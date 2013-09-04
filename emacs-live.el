db1e14dfb988b3899e397e26b74ea262501ff0a3
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-09-04 10:04:05 +0100

    upgrade emacs-live version.

--- emacs-live.el
+++ emacs-live.el
@@ -3,6 +3,7 @@
                    live/git-pack
                    live/clojure-pack
                    live/lang-pack
+                   live/org-pack
                    live/bindings-pack))
 
 (live-add-packs '(~/.live-packs/solarized-pack ~/.live-packs/sundbp-pack))
