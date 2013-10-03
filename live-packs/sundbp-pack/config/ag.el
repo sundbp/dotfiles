a152b21566e67393ce76bad32a6b4e1655bf12d6
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-10-03 11:13:59 +0100

    avoid searching through auto-generated content.

--- live-packs/sundbp-pack/config/ag.el
+++ live-packs/sundbp-pack/config/ag.el
@@ -3,4 +3,4 @@
 (defalias 'agp 'ag-project-at-point)
 (defalias 'agrp 'ag-regexp-project-at-point)
 (setq ag-highlight-search t)
-(setq ag-arguments (list "--smart-case" "--nogroup" "--column" "--skip-vcs-ignores" "--"))
+(setq ag-arguments (list "--smart-case" "--nogroup" "--column" "--skip-vcs-ignores" "--ignore generated/*" "--"))
