54232a797f0b4ea2dfabc57dd91fae58de3edee2
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-12-02 10:23:37 +0000

    version updates and better stacktraces.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,16 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-immutant "1.0.1"]
+                  [lein-immutant "1.1.0"]
                   [lein-ancient "0.4.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.3"]
-                       [slamhound "1.3.3"]
-                       [nrepl-inspect "0.3.0"]]
+                       [slamhound "1.5.0"]
+                       [nrepl-inspect "0.4.1"]
+                       [clj-stacktrace "0.2.7"]
+                       [repetition-hunter "1.0.0"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly (deref new))))]
         :repl-options {:nrepl-middleware [inspector.middleware/wrap-inspect]}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :mirrors {"central" {:name "central-proxy"
