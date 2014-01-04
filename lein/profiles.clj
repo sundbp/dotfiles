38c7aa643811dc456e99f80ec1334ecd0e8affb3
Patrik Sundberg <patrik.sundberg@gmail.com>
2014-01-04 16:53:02 +0000

    cooler stacktraces and add spyscope.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,17 +1,15 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-immutant "1.1.0"]
+                  [lein-immutant "1.1.1"]
                   [lein-ancient "0.4.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.3"]
                        [slamhound "1.5.0"]
                        [nrepl-inspect "0.4.1"]
-                       [clj-stacktrace "0.2.7"]
-                       [repetition-hunter "1.0.0"]]
-        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            'print-cause-trace)
-                           new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           'pst)]
-                       (alter-var-root orig (constantly (deref new))))]
-        :repl-options {:nrepl-middleware [inspector.middleware/wrap-inspect]}
+                       [io.aviso/pretty "0.1.8"]
+                       [repetition-hunter "1.0.0"]
+                       [spyscope "0.1.4"]]
+        :injections [(require 'spyscope.core)]
+        :repl-options {:nrepl-middleware [inspector.middleware/wrap-inspect
+                                          io.aviso.nrepl/pretty-middleware]}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :mirrors {"central" {:name "central-proxy"
                             :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/central/"}
