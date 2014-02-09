f49f1f1651d835bcecb830dcf9c5d6969bb8e8db
Patrik Sundberg <patrik.sundberg@gmail.com>
2014-02-09 22:20:05 +0000

    ensure right load order of packs.

--- emacs-live.el
+++ emacs-live.el
@@ -1,9 +1,9 @@
 (live-use-dev-packs)
 
 (live-use-packs '(dev/foundation-pack
+                  dev/power-pack
                   dev/clojure-pack
                   dev/lang-pack
-                  dev/power-pack
                   dev/git-pack
                   dev/org-pack
                   dev/bindings-pack))
