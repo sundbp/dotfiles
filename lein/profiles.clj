cc6315c861b617181398814d3b0a5e1bd40fd03e
Patrik Sundberg <patrik.sundberg@gmail.com>
2015-04-13 15:56:34 +0100

    lein: updates profiles.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,22 +1,17 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-immutant "1.1.1"]
-                  [lein-ancient "0.4.2"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.3"]
-                       [slamhound "1.5.0"]
-                       [nrepl-inspect "0.4.1"]
-                       [io.aviso/pretty "0.1.8"]
-                       [repetition-hunter "1.0.0"]
-                       [spyscope "0.1.4"]]
+{:user {:plugins [[lein-ancient "0.4.2" :exclusions [org.clojure/clojure commons-codec]]
+                  [cider/cider-nrepl "0.8.2"]
+                  [refactor-nrepl "0.2.2"]]
+        :dependencies [[slamhound "1.5.5"]
+                       [spyscope "0.1.5"]]
         :injections [(require 'spyscope.core)]
-        :repl-options {:nrepl-middleware [inspector.middleware/wrap-inspect
-                                          io.aviso.nrepl/pretty-middleware]}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :mirrors {"central" {:name "central-proxy"
-                            :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/central/"}
-                 #"clojars" {:name "clojars-proxy"
-                             :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/clojars/"}}
+                           :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/central/"}
+                 "clojars" {:name "clojars-proxy"
+                            :url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/clojars/"}}
         :deploy-repositories [["snapshots" {:url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/snapshots/"
-                                            :update :always}]
+                                            :username "tulos"
+                                            :password "tulos123"}]
                               ["releases" {:url "http://nexus.vlan.tuloscapital.com:8081/nexus/content/repositories/releases/"
-                                           :update :always}]]
-        }}
+                                           :username "tulos"
+                                           :password "tulos123"}]]}}
