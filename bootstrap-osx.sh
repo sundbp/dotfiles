2b0d3a40998801d8ae12c8524b72fd681606f5bf
Patrik Sundberg <patrik.sundberg@gmail.com>
2017-09-06 11:53:16 +0100

    ignore fisherman packages.

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install homebrew
-#ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 
 # install packages
 brew install coreutils grc encfs vim keychain gpg-agent thefuck
