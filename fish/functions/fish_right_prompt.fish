f4a6bd40c703f1a6bc3e0f7f9f2b2e67ba1d7b1e
Patrik Sundberg <patrik.sundberg@gmail.com>
2017-11-08 10:20:51 +0000

    clean up fish prompt a little.

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,15 +1,19 @@
 function fish_right_prompt
-  set -l code $status
-  if test $CMD_DURATION -gt 3000
-    if test $code -ne 0
-      set_color -o red
-    else
-      set_color -o 484
+  if test $TERM = "dumb"
+    return
+  else
+    set -l code $status
+    if test $CMD_DURATION -gt 3000
+      if test $code -ne 0
+        set_color -o red
+      else
+        set_color -o 484
+      end
+      printf "~%.1fs " (math "$CMD_DURATION / 1000")
+      set_color -o normal
     end
-    printf "~%.1fs " (math "$CMD_DURATION / 1000")
+    set_color 666
+    printf "%s" (command date +%H:%M:%S)
     set_color -o normal
   end
-  set_color 666
-  printf "%s" (command date +%H:%M:%S)
-  set_color -o normal
 end
