7b3b19a352f6ad0bf622a89cc38ffbabed328f3c
Patrik Sundberg <sundbp@hulk.vlan.tuloscapital.com>
2017-12-04 22:05:58 +0000

    update lein profile.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,12 +1,14 @@
-{:repl {:dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
-        :repl-options {:timeout 120000}}
- :user {:plugins [[com.billpiel/sayid "0.0.10"]]
-        :dependencies [[jsofra/data-scope "0.1.0"]]
+{:repl {:dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                       [jsofra/data-scope "0.1.0"]]
+        :repl-options {:timeout 120000}
+        :plugins [;;[com.billpiel/sayid "0.0.10"]
+                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]
+                  [refactor-nrepl "2.4.0-SNAPSHOT"]]
         :injections [(require 'data-scope.charts)
                      (require 'data-scope.graphs)
                      (require 'data-scope.inspect)
-                     (require 'data-scope.pprint)]
-        :deploy-repositories [
+                     (require 'data-scope.pprint)]}
+ :user {:deploy-repositories [
                               ;; ["snapshots" {:url "http://artifactory.default.svc.cluster.local/artifactory/libs-snapshot-local/"
                               ;;               :username "admin"}]
                               ;; ["releases" {:url "http://artifactory.default.svc.cluster.local/artifactory/libs-release-local/"
@@ -14,5 +16,5 @@
                               ["releases" {:url "http://10.10.10.5:8081/artifactory/libs-release-local/"
                                            :username "sundbp"}]
                               ["snapshots" {:url "http://10.10.10.5:8081/artifactory/libs-snapshot-local/"
-                                           :username "sundbp"}]]
-        }}
+                                            :username "sundbp"}]]}
+ }
