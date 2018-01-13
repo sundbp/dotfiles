5948e9b2a76b5ad1d895287104fae3f7b7f6d6aa
Patrik Sundberg <patrik.sundberg@gmail.com>
2018-01-13 00:18:25 +0000

    rbenv: add ruby-build plugin.

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -148,5 +148,9 @@ fi
 # nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
+# ruby-build
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
 echo ""
 echo "Setup of your dot files completed!"
